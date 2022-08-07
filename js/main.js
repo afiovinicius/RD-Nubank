4538bcebec7ebe3908a66497500ff5205330dc41
afiovinicius <afiovinicius@gmail.com>
2022-08-07 00:01:38 -0300

    html e styles inicial finalizado

---
+++ js/main.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal(".ef-i-down", {
+  origin: "top",
+  distance: "20px",
+  duration: 1000,
+  easing: "ease",
+  interval: 300,
+});
+
+ScrollReveal().reveal(".ef-i-left", {
+  origin: "left",
+  distance: "20px",
+  duration: 1000,
+  easing: "ease",
+  interval: 300,
+});
+
+ScrollReveal().reveal(".ef-i-right", {
+  origin: "right",
+  distance: "20px",
+  duration: 1000,
+  easing: "ease",
+  interval: 300,
+});
